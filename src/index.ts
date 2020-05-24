2cc77400b182c0f6ec2d78b893b8646abd2f0c2f
Binarysearch <desenlace.net@gmail.com>
2020-05-24 20:45:53 +0200

    export interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { AplicationStatusService } from "./service/application-status-service";
 export { StatusServer } from "./server/status-server";
 export { DefaultStatusProvider } from "./service/default-status-provider";
-
+export { ReplicaMessage, ReplicaStatus, AplicationService, AplicationStatus } from './interface/aplication-status';
