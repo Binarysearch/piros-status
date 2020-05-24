27c52660fa579905cd17c8c79cd5b4015df42341
Binarysearch <desenlace.net@gmail.com>
2020-05-24 10:18:47 +0200

    socket object stream

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,18 @@
 import { Server, Socket, AddressInfo } from 'net';
+import { SocketObjectStream } from './app/socket-object-stream';
 
 
 const server: Server = new Server((socket: Socket) => {
     console.log('Socket connected');
 
-    let sobrante: Buffer;
+    const sender = new SocketObjectStream(socket);
 
-    socket.on('data', (data) => {
-        console.log(`Received: ${data.length} bytes`);
-        let finished = false;
-        let pos = 0;
+    let i = 0;
 
-        let a=0;
-        while (!finished) {
-            const ceroPos = data.indexOf(0, pos);
-
-            if (ceroPos !== -1) {
-                if (sobrante) {
-                    a++;
-                    console.log('Messaje: ', Buffer.concat([sobrante, data.slice(pos, ceroPos)]).toString());
-                    sobrante = null;
-                } else {
-                    a++
-                    console.log('Messaje: ', data.slice(pos, ceroPos).toString());
-                }
-                
-                pos = ceroPos + 1;
-            }
-            
-            finished = ceroPos === -1;
-
-            
-        }
-
-        if (pos < data.length) {
-            sobrante = data.slice(pos);
-        }
-        
+    sender.getReceivedMessages().subscribe((message) => {
+        console.log('message', message);
+        i++;
+        console.log(i);
     });
 });
 
@@ -46,35 +22,14 @@ console.log('Listening');
 
 
 const client: Socket = new Socket();
+const sender = new SocketObjectStream(client);
+
+sender.getReceivedMessages().subscribe((message) => {
+    console.log(message);
+});
 
 client.connect(1337, '127.0.0.1', () => {
-	
     for (let i = 0; i < 10000; i++) {
-        write({ a:Math.random(), b: 'oo' }, client);
+        sender.sendMessage({ a:Math.random(), b: 'oo' });
     }
 });
-
-client.on('data', (data) => {
-	console.log('Received: ' + data);
-	
-});
-
-client.on('close', () => {
-	console.log('Connection closed');
-});
-
-
-
-function write(data: any, socket: Socket) {
-    if (Math.random() > 0.5) {
-        socket.write(JSON.stringify(data));
-        socket.write(new Uint8Array([0]));
-    } else {
-        socket.write(JSON.stringify(data).slice(0,5));
-        
-        socket.write(JSON.stringify(data).slice(6));
-        socket.write(new Uint8Array([0]));
-        
-    }
-    
-}
